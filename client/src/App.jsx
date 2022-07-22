23397efe1e4d4c6e564b84c74bd9af7de6419bf3
huskyhehe <zhou.he1@northeastern.edu>
2022-07-22 13:57:30 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import RecordList from "./components/RecordList";
 import Edit from "./components/Edit";
 import Create from "./components/Create";
@@ -10,7 +10,7 @@ import Create from "./components/Create";
 function App() {
 	return (
 		<div className="App">
-			<Navbar />
+			<NavBar />
 			<Routes>
 				<Route exact path="/" element={<RecordList />} />
 				<Route path="/edit/:id" element={<Edit />} />
